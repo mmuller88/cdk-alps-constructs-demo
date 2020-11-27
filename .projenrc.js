eb20580129a204ebd9c59905f81838e2f7283a54
martin.mueller <m.mueller@unimed.de>
2020-11-27 07:15:40 +0100

    feat: init demo repo

---
+++ .projenrc.js
@@ -0,0 +1,21 @@
+const { AwsCdkTypeScriptApp } = require('projen');
+
+const project = new AwsCdkTypeScriptApp({
+  authorAddress: "damadden88@googlemail.com",
+  authorName: "Martin Mueller",
+  name: "cdk-alps-constructs-demo",
+  defaultReleaseBranch: "main",
+  cdkVersion: "1.75.0",
+  repository: "https://github.com/mmuller88/cdk-alps-constructs-demo.git",
+  keywords: [
+    'cdk',
+    'aws',
+    'alps',
+    'appsync',
+    'graphql',
+    'apigateway',
+  ],
+
+});
+
+project.synth();
